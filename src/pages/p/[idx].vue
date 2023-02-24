29dbddd0b32109936f6b62cee5ee32487038f5c4
c1llo <47639656+c1llo@users.noreply.github.com>
2023-02-24 16:43:32 +0000

    refactor: use ref for image

--- src/pages/p/[idx].vue
+++ src/pages/p/[idx].vue
@@ -1,6 +1,6 @@
 <template>
     <main class="antialiased transform-gpu w-full h-full absolute inset-0 select-none bg-background">
-        <ImageGallery :image="image" />
+        <ImageGallery :image="image" v-if="image" />
     </main>
 </template>
 
@@ -8,27 +8,31 @@
 import ImageGallery from "@/components/image-gallery.vue";
 import { getImages } from "@/composables";
 import { useRoute } from "vue-router";
+import type { Image } from "@/types";
 
 definePageMeta({
     pageTransition: { name: "page", mode: "out-in", duration: 250 },
 });
 
 const route = useRoute();
+const currentImage = useCurrentImage();
 const images = getImages();
 
-const image = images.find((image) => image.idx == (route.params.idx as unknown as number));
+const image = ref<Image | undefined>(images.find((image) => image.idx == (route.params.idx as unknown as number)));
 
-if (!image) {
+if (!image.value) {
     throw createError({
         statusCode: 404,
         message: "Image not found",
     });
 }
 
+currentImage.value = image.value;
+
 useHead({
     meta: [
-        { name: "twitter:image", content: `${image.src}` },
-        { property: "og:image", content: `${image.src}` },
+        { name: "twitter:image", content: `${image.value.src}` },
+        { property: "og:image", content: `${image.value.src}` },
     ],
 });
 </script>
