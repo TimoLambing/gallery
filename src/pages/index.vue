baa02d4f240ceeb83f7d3bc6b257d04b75c60432
c1llo <47639656+c1llo@users.noreply.github.com>
2023-02-24 00:30:20 +0000

    feat(css): grid layout columns

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-    <main class="antialiased transform-gpu w-full min-h-screen flex items-start justify-center select-none">
-        <div class="w-full h-full flex items-center justify-center p-4">
-            <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4 w-full max-w-mw">
+    <main class="antialiased transform-gpu w-full min-h-screen flex items-start justify-center select-none p-4">
+        <div class="w-full h-full flex items-center justify-center max-w-mw">
+            <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
                 <ImageItem
                     v-for="image in images"
                     :key="image.idx"
