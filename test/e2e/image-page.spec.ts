c142daa8e4c7acf53d5a42052e3668eb89063ee2
c1llo <47639656+c1llo@users.noreply.github.com>
2023-02-25 17:19:29 +0000

    test(refactor): wait for button transition

--- test/e2e/image-page.spec.ts
+++ test/e2e/image-page.spec.ts
@@ -4,8 +4,10 @@ test.describe("Image Page", () => {
     test("it matches snapshot", async ({ page }) => {
         await page.goto("/p/0");
 
-        const selector = '[data-testid="close"]';
-        await page.waitForSelector(selector, { timeout: 5000 });
+        await page.getByTestId("close");
+
+        //wait for transition of buttons (max 250ms)
+        await page.waitForTimeout(500);
 
         expect(await page.screenshot()).toMatchSnapshot();
     });
